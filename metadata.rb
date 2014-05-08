31145ca2b739bf0f465854f433b4fd407c7c91fe
Steve Buzonas <steve@fancyguy.com>
2014-05-07 20:12:15 -0400

    bumped version, removed ohai dependency

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,9 @@ maintainer_email 'devops@fancyguy.com'
 license          'Apache 2.0'
 description      'Installs/Configures chef-solo-node'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 recipe 'chef-solo-node', 'Captures necessary information from the generated node to persist in a data bag to be searched with chef-solo-search'
-
-depends 'ohai', '~> 1.1'
 
 supports 'amazon'
 supports 'centos'
